d035e9bf8b780ba10365c164aa32c725f6aaf328
loibh <loi.bui@orientsoftware.net>
2016-08-10 10:36:06 +0700

    debug

--- test-main.js
+++ test-main.js
@@ -1,9 +1,9 @@
 if (!Object.hasOwnProperty('name')) {
   Object.defineProperty(Function.prototype, 'name', {
-    get: function() {
+    get: function () {
       var matches = this.toString().match(/^\s*function\s*(\S*)\s*\(/);
       var name = matches && matches.length > 1 ? matches[1] : "";
-      Object.defineProperty(this, 'name', {value: name});
+      Object.defineProperty(this, 'name', { value: name });
       return name;
     }
   });
@@ -12,11 +12,11 @@ if (!Object.hasOwnProperty('name')) {
 // Turn on full stack traces in errors to help debugging
 Error.stackTraceLimit = Infinity;
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
 // Cancel Karma's synchronous start,
 // we will call `__karma__.start()` later, once all the specs are loaded.
-__karma__.loaded = function() {};
+__karma__.loaded = function () { console.log("Karma - Loaded") };
 
 // Load our SystemJS configuration.
 System.config({
@@ -78,27 +78,29 @@ Promise.all([
   testing.setBaseTestProviders(testingBrowser.TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
     testingBrowser.TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS);
 
-}).then(function() {
+}).then(function () {
   return Promise.all(
     Object.keys(window.__karma__.files) // All files served by Karma.
-    .filter(onlySpecFiles)
-    .map(file2moduleName)
-    .map(function(path) {
-      return System.import(path).then(function(module) {
-        if (module.hasOwnProperty('main')) {
-          module.main();
-        } else {
-          throw new Error('Module ' + path + ' does not implement main() method.');
-        }
-      });
-    }));
+      .filter(onlySpecFiles)
+      .map(file2moduleName)
+      .map(function (path) {
+        return System.import(path).then(function (module) {
+          console.log(path);
+          if (module.hasOwnProperty('main')) {
+            module.main();
+          } else {
+            throw new Error('Module ' + path + ' does not implement main() method.');
+          }
+        });
+      }));
 })
-.then(function() {
-  __karma__.start();
-}, function(error) {
-  console.error(error.stack || error);
-  __karma__.start();
-});
+  .then(function () {
+    console.log("Karma - Started")
+    __karma__.start();
+  }, function (error) {
+    console.error(error.stack || error);
+    __karma__.start();
+  });
 
 function onlySpecFiles(path) {
   // check for individual files, if not given, always matches to all
