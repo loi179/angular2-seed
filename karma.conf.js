0c7231180703dab1254509dd2126fdce92e7f944
loibh <loi.bui@orientsoftware.net>
2016-08-10 13:33:45 +0700

    update config

--- karma.conf.js
+++ karma.conf.js
@@ -8,10 +8,10 @@ module.exports = function (config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: './',
+    basePath: '',
 
     captureTimeout: 60000,
-    browserNoActivityTimeout = 90000,
+    browserNoActivityTimeout: 90000,
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
